4f432e895fa87671b3bae1487c4c855f0e417a29
Hilmi K <90126618+raiho80@users.noreply.github.com>
2023-01-14 17:14:00 +0800

    Update classify_streamlit.py

--- classify_streamlit.py
+++ classify_streamlit.py
@@ -2,6 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+
 import streamlit as st
 
 # streamlit containers
